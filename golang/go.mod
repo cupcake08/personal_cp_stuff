c523a1ba11589f9d872f610671dc0a7768742388
cupcake08 <bhankhariaa@gmail.com>
2022-12-19 22:47:13 +0530

    :fire: basic ready

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/cupcake08/cp_scripts
+
+go 1.19
--- my_script.sh
+++ my_script.sh
@@ -3,7 +3,7 @@ echo "runnig script"
 
 read folderName number
 
-go run golang/script.go $folderName $number
+go run /home/ankit/CP/golang/script.go $folderName $number
 
 read -p "do you want to open vs code (y/n)" openVsCode
 

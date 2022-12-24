6e9e4e152b0741c7a0eb0e22482680069540001d
cupcake08 <bhankhariaa@gmail.com>
2022-12-24 22:09:05 +0530

    :recycle: Refactor Script Code

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 platform=$(gum choose "Codeforces" "Custom")
 echo "Platform choosen > $(gum style --foreground 212 $platform)"
 
+# Add to gitignore
 add_to_gitignore() {
     if grep -F -q "$1/" .gitignore; then
         echo "Already added to gitignore"
@@ -11,17 +12,29 @@ add_to_gitignore() {
     fi
 }
 
+# Open editor
+editor_action() {
+    case $platform in
+        "Codeforces")
+            $1 codeforces_contest_$contestId
+        ;;
+        "Custom")
+            $1 $folderName
+        ;;
+    esac
+}
+
 case $platform in
     "Codeforces")
         contestId=$(gum input --placeholder "Enter contest id")
         cd golang
-        go run /home/ankit/CP/golang/script.go $platform $contestId &
-        BG_PID=$!
-        while kill -0 $BG_PID 2>/dev/null; do
+        go run script.go $platform $contestId &
+        BACK_PID=$!
+        while kill -0 $BACK_PID 2>/dev/null; do
             gum spin -s line --title "work in progress..." -- sleep 1
         done
         cd ..
-        add_to_gitignore "codeforces_contest_$contestId"
+        add_to_gitignore codeforces_contest_$contestId
     ;;
     "Custom")
         folderName=$(gum input --placeholder "Enter folder name")
@@ -29,25 +42,13 @@ case $platform in
         number=$(gum input --placeholder "Enter number of files to create")
         echo "Number of files is $(gum style --foreground 212 $number)"
         cd golang
-        go run /home/ankit/CP/golang/script.go $platform $folderName $number
+        go run script.go $platform $folderName $number
         cd ..
         add_to_gitignore $folderName
     ;;
 esac
 
 gum style --foreground '#FE987B' "(>‿◠)✌ Yay! Done"
-
-editor_action() {
-    cmd=$1
-    case $platform in
-        "Codeforces")
-            $cmd codeforces_contest_$contestId
-        ;;
-        "Custom")
-            $cmd $folderName
-        ;;
-    esac
-}
 
 if gum confirm "Want to open editor?"; then
     editor=$(gum choose "Vs Code" "Vim" "Neovim")
